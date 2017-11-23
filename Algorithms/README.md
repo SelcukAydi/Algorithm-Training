2ddc6f2449391cfa3a4c3e226da6e5fbea97b911
selcukaydi <slck.aydi@gmail.com>
2017-11-23 13:08:22 +0300

    Heap Sort algorithm added.

--- Algorithms/README.md
+++ Algorithms/README.md
@@ -1,3 +1,1 @@
-This implementation takes an number which has max 4 digits and it must be a positive number.
-This algorithm divide the digits of this number and store them into a vector. All the operations
-made by maths.
+This file is a read me file.
---
+++ Algorithms/SortingAlgorithms/HeapSort.cpp
@@ -0,0 +1,86 @@
+//============================================================================
+// Name        : HeapSort.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void HeapIFY(int A[], int i);
+void BuildMaxHeap(int A[]);
+void HeapSort(int A[]);
+int Left(int i);
+int Right(int i);
+void SwapElements(int i, int largest, int A[]);
+void printArray(int A[], int length);
+
+int length = 9;
+int heapSize = length;
+
+int main() {
+
+	int A[10] = { 0,4,3,10,7,1,2,8,14,16 };
+	HeapSort(A);
+	printArray(A, length);
+
+	cout<<"\nDone"<<endl;
+
+	return 0;
+}
+
+void HeapIFY(int A[], int i) {
+	int left = Left(i);
+	int right = Right(i);
+	int largest;
+
+	if(left <= heapSize && A[left] > A[i])
+		largest = left;
+	else
+		largest = i;
+
+	if(right <= heapSize && A[right] > A[largest])
+		largest = right;
+
+	if(largest != i){
+		SwapElements(i, largest, A);
+		HeapIFY(A, largest);
+	}
+}
+
+void BuildMaxHeap(int A[]) {
+	for(int i = length / 2; i > 0; i--) {
+		HeapIFY(A, i);
+	}
+}
+
+void HeapSort(int A[]) {
+	BuildMaxHeap(A);
+	for(int i = length; i >= 2; i--) {
+		SwapElements(i, 1, A);
+		heapSize--;
+		HeapIFY(A, 1);
+	}
+}
+
+int Left(int i) {
+	return (i * 2);
+}
+
+int Right(int i) {
+	return Left(i) + 1;
+}
+
+void SwapElements(int i, int largest, int A[]) {
+	int tmp = A[i];
+	A[i] = A[largest];
+	A[largest] = tmp;
+}
+
+void printArray(int A[], int length) {
+	for(int i = 0; i <= length; i++) {
+		cout<<A[i]<<" ";
+	}
+}
