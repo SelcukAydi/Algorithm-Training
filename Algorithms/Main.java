9d968b46645272a14c43922cee0ede475d75ac4a
WHSoftware <slck.aydi@gmail.com>
2017-11-28 00:52:35 +0300

    Edited

--- Algorithms/Main.java
+++ Algorithms/Main.java
@@ -1,11 +1,28 @@
+package com.sia;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    PriorityQueue queue = new PriorityQueue();
+    queue.Insert(11);
+    queue.Insert(12);
+    queue.Insert(13);
+    queue.Insert(14);
+    queue.Insert(15);
+    queue.Insert(16);
+    queue.Insert(17);
 
+    queue.PrintArray();
 
-
-
+    queue.ExtractMax();
+    queue.PrintArray();
+    queue.ExtractMax();
+    queue.PrintArray();
+      queue.ExtractMax();
+      queue.PrintArray();
+      queue.ExtractMax();
+      queue.PrintArray();
 
   }
 }
--- Algorithms/PriorityQueue.java
+++ Algorithms/PriorityQueue.java
@@ -1,3 +1,5 @@
+package com.sia;
+
 import java.util.ArrayList;
 
 public class PriorityQueue {
@@ -10,6 +12,31 @@ public class PriorityQueue {
         HeapSize = 0;
     }
 
+    public void Insert(int element) {
+        queue.add(element);
+        HeapSize++;
+        int hasAdded = queue.size() - 1;
+        int parent = getParent(hasAdded);
+
+        while(queue.get(parent) < queue.get(hasAdded)) {
+            ExchangeNodes(parent, hasAdded);
+            hasAdded = parent;
+            parent = getParent(hasAdded);
+        }
+    }
+
+    public int ExtractMax() {
+        int result = getMax();
+        queue.set(0, queue.get(HeapSize - 1));
+        HeapSize--;
+        MaxHeapFIY(0);
+        return result;
+    }
+
+    public int getMax() {
+        return queue.get(0);
+    }
+
     public int getMaxValue() {
         return queue.get(0);
     }
@@ -17,13 +44,13 @@ public class PriorityQueue {
     private void MaxHeapFIY(int parent) {
         int leftChild   = getLeftChild(parent);
         int rightChild  = getRightChild(parent);
-        int largestNode = queue.get(parent);
+        int largestNode = parent;
 
-        if(leftChild <= HeapSize && queue.get(leftChild) > queue.get(parent)) {
+        if(leftChild < HeapSize && queue.get(leftChild) > queue.get(parent)) {
             largestNode = leftChild;
         }
 
-        else if(rightChild <= HeapSize && queue.get(rightChild) > queue.get(largestNode)) {
+        if(rightChild < HeapSize && queue.get(rightChild) > queue.get(largestNode)) {
             largestNode = rightChild;
         }
 
@@ -39,11 +66,23 @@ public class PriorityQueue {
         queue.set(largestNode, tmp);
     }
 
+    public void PrintArray() {
+        int i = 0;
+        for(i = 0; i < HeapSize - 1; i++) {
+            System.out.print(queue.get(i) + "  ");
+        }
+        System.out.println(queue.get(i));
+    }
+
     private int getLeftChild(int index) {
         return index * 2 + 1;
     }
 
     private int getRightChild(int index) {
         return getLeftChild(index) + 1;
+    }
+
+    private int getParent(int child) {
+        return (child - 1) / 2;
     }
 }
